9ca96c869ddd5323320c4a1919b186cb1eeb0773
VicvekSr9485 <olamideoso65@gmail.com>
2023-09-03 13:09:06 +0100

    add google login

---
+++ verdeharvest_frontend/src/container/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import verdeharvest from '../assets/verdeharvest-background.png';
+//import RegistrationForm from '../components/Register'
+
+function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-cover" style={{ backgroundImage: `url(${verdeharvest})` }}>
+
+    </div>
+  )
+}
+
+export default Home
